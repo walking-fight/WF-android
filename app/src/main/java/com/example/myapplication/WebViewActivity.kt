cd6d9913ccee2d89ed432d218f09f71a275ebea5
dongtony12 <dongtony0523@naver.com>
2023-05-08 08:28:59 +0900

    WebView 설정

---
+++ app/src/main/java/com/example/myapplication/WebViewActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class WebViewActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        webView = findViewById(R.id.webView)
+        setupWebView()
+    }
+
+    private fun setupWebView() {
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://your-react-project-url.com")
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
